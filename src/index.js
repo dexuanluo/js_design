2fc1c99d834b21475968405deccd0983edd0cd94
Dexuan <ldxkennedy333@gmail.com>
2020-11-28 22:22:38 -0600

    babel

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
-alert(100);
+class Person{
+    constructor(name){
+        this.name = name
+    }
+
+    get_name(){
+        return this.name
+    }
+}
+
+let p = Person("haha")
+
+alert(p.get_name())
+
 
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -8,11 +8,19 @@ module.exports = {
 		path: __dirname,
 		filename: './release/bundle.js'
 	},
+
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: './index.html'
 		})
 	],
+	module : {
+		rules : [{
+			test : /\.js?$/,
+			exclude : /(node_modules)/,
+			loader : 'babel-loader'
+		}]
+	},
 	devServer: {
 		contentBase: path.join(__dirname, './release'),
 		open: true,
